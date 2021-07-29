9222b0684ada328f7735f69da409b953b09c65ff
Kevin852 <kevinshah852@gmail.com>
2021-07-29 11:18:58 +0530

    first commit

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace JenkinsApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello Kevin!");
+            Console.WriteLine("Hello User!");
+        }
+    }
+}
